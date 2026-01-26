84f09f4357534f20f04e3e68e452de9b7fe881a5
kouji <kouji@hero555noMac-mini-3.local>
2026-01-26 10:29:18 +0900

    first commit

---
+++ nix/darwin-configuration.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+
+{
+  # Nix の設定
+  # Determinate Nix を使用しているため、nix-darwin の Nix 管理を無効化
+  nix.enable = false;
+
+  # Homebrew の設定
+  homebrew = {
+    enable = true;
+
+    # brew install で入れるパッケージ
+    brews = [
+      "gh"
+      "jq"
+    ];
+
+    # brew install --cask で入れるアプリケーション（必要に応じて追加）
+    casks = [
+      # "google-chrome"
+      # "visual-studio-code"
+    ];
+
+    # Mac App Store からインストールするアプリ（必要に応じて追加）
+    # masApps = {
+    #   "Xcode" = 497799835;
+    # };
+
+    # nix-darwin で管理されていないパッケージの扱い
+    onActivation = {
+      # "zap" にすると管理外のパッケージを削除
+      # "uninstall" にすると管理外のパッケージをアンインストール
+      # "none" にすると何もしない
+      cleanup = "none";
+
+      # 自動的に brew update を実行
+      autoUpdate = true;
+
+      # 自動的に brew upgrade を実行
+      upgrade = true;
+    };
+  };
+
+  # システム設定
+  system = {
+    # 設定変更を追跡するためのバージョン
+    stateVersion = 5;
+
+    # darwin-rebuild を実行するユーザー（homebrew.enable に必要）
+    primaryUser = "kouji";
+  };
+
+  # プログラムの設定
+  programs = {
+    # デフォルトシェルの設定（必要に応じて）
+    zsh.enable = true;
+  };
+
+  # Nix パッケージでインストールするもの
+  environment.systemPackages = with pkgs; [
+    mise
+  ];
+}
---
+++ nix/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Nix Darwin configuration with Homebrew";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nix-darwin, ... }:
+  let
+    # ホスト名を取得するか、デフォルト値を使用
+    hostname = "hero555noMac-mini-3";
+  in
+  {
+    darwinConfigurations.${hostname} = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";  # Apple Silicon の場合。Intel Mac の場合は "x86_64-darwin"
+      modules = [
+        ./darwin-configuration.nix
+      ];
+    };
+
+    # デフォルト設定（任意のホスト名で使用可能）
+    darwinConfigurations.default = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ./darwin-configuration.nix
+      ];
+    };
+  };
+}
